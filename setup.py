bd691d31b9ec2fd7e3beb1d55e5ba155335fed4f
Alex Misstear <amisstea@redhat.com>
2016-12-16 09:58:27 -0500

    Basic project structure

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+
+with open('LICENSE') as fh:
+    license = fh.read()
+
+with open('README.md') as fh:
+    readme = rh.read()
+
+setup(
+    name='cloudimg',
+    version='0.1.0',
+    author='Alex Misstear',
+    author_email='amisstea@redhat.com',
+    description=('Services for building and releasing products in cloud '
+                 'environments'),
+    long_description=readme,
+    license=license,
+    url='https://gitlab.cee.redhat.com/rad/cloud-image',
+    packages=find_packages(exclude=('tests', 'bin', 'docs'))
+)
