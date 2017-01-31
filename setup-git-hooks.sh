d30a16266282a477693c18428ab19cb202df8bb1
Alex Misstear <amisstea@redhat.com>
2017-01-31 09:19:42 -0500

    Basic flake8 pre-commit hook

---
+++ setup-git-hooks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for hook in git-hooks/*;do
+    echo "Setting up git hook for $hook"
+    basename=$(basename $hook)
+    chmod +x $hook
+    ln -sf ../../${hook} .git/hooks/$basename
+done
+
+echo "Git hooks setup complete"
