35822c7ce9aea153047a1b95f739f49077c681a5
Alex Misstear <amisstea@redhat.com>
2017-02-13 14:59:37 -0500

    Removed config
    Since cloudimg will consist of python modules and no services, it
    no longer makes sense to include a config. This should make the
    project more flexible to a wider audience. Any services required
    will be maintained within their own repositories.

--- cloudimg/aws.py
+++ cloudimg/aws.py
@@ -1,11 +1,11 @@
 import logging
+from uuid import uuid1
 
 from libcloud.compute.providers import get_driver as get_compute_driver
 from libcloud.storage.providers import get_driver as get_storage_driver
 from libcloud.compute.types import Provider as ComputeProvider
 from libcloud.storage.types import Provider as StorageProvider
 
-from cloudimg import config
 from cloudimg.common import BaseService
 
 log = logging.getLogger(__name__)
@@ -14,21 +14,32 @@ log = logging.getLogger(__name__)
 class AWSService(BaseService):
     """
     AWS cloud provider service.
+
+    Args:
+        access_id (str): AWS account access ID
+        secret_key (str): AWS account secret access key
+        region (str, optional): AWS region for compute operations
     """
 
-    def __init__(self):
+    # Auto generated storage container if none is provided on upload
+    STORAGE_CONTAINER = 'cloudimg-' + str(uuid1())
+
+    def __init__(self, access_id, secret_key, region='us-east-1'):
         StorageDriver = get_storage_driver(StorageProvider.S3)
-        storage = StorageDriver(config.AWS_ACCESS_ID,
-                                secret=config.AWS_SECRET_KEY)
+        storage = StorageDriver(access_id, secret_key)
 
         ComputeDriver = get_compute_driver(ComputeProvider.EC2)
-        compute = ComputeDriver(config.AWS_ACCESS_ID,
-                                config.AWS_SECRET_KEY,
-                                config.AWS_DEFAULT_REGION)
+        compute = ComputeDriver(access_id, secret_key, region)
 
         super(AWSService, self).__init__(storage, compute)
 
-    def upload(self, image, name=None):
-        return self.upload_to_container(image,
-                                        config.AWS_STORAGE_CONTAINER,
-                                        name=name)
+    def upload(self, image, name=None, container=STORAGE_CONTAINER):
+        """
+        Refer to ``cloudimg.common.BaseService.upload`` for more info.
+
+        Args:
+            container (str, optional): The storage container where the image
+                                       will be stored. Auto generated if not
+                                       specified.
+        """
+        return self.upload_to_container(image, container, name=name)
