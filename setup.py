05964fc021900b3d546a4445e268fd7aa17f4578
Rohan McGovern <rmcgover@redhat.com>
2020-03-04 11:27:20 +1000

    Release 0.2.7
    Initial (manual) release to PyPI.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cloudimg',
-    version='0.2.6',
+    version='0.2.7',
     author='Alex Misstear',
     author_email='amisstea@redhat.com',
     description=('Services for building and releasing products in cloud '
