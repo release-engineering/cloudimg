4ac4821e6268f0f3f8e1ed2f79209bb5257a5dd1
Robert Bikar <rbikar@redhat.com>
2026-02-04 11:52:15 +0100

    Release 1.16.0
    - extra update to GH actions (versions bump)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cloudimg',
-    version='1.15.0',
+    version='1.16.0',
     author='Alex Misstear',
     author_email='amisstea@redhat.com',
     description=('Services for building and releasing products in cloud '
